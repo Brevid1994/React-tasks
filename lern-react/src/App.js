c1b4ce7d0f87f6e632ef70bbeaa387c69e5c33f6
Brevid1994 <72521095+Brevid1994@users.noreply.github.com>
2021-03-01 20:31:15 +0300

    doc: add ClassWork2

--- lern-react/src/App.js
+++ lern-react/src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
+import TextChanges from './Components/ClassWork2/TextChanges.js';
+import ChengeSymbol from './Components/ClassWork2/ChengeSymbol.js';
+
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TextChanges text='asd' size='25' color='red' />
+      <TextChanges text='aasdassd' size='20' color='black' />
+      <TextChanges text='aasdassd' size='30' color='green' />
+      <TextChanges text='asasdasd' size='15' color='blue' underline ='underline'/> 
+      <ChengeSymbol/>
     </div>
   );
 }
