2c2efd5b155446e2c71ca7bf488089f6726eceee
Brevid1994 <72521095+Brevid1994@users.noreply.github.com>
2021-03-01 20:53:19 +0300

    init: complite Square task

--- lern-react/src/Components/Homework2/Square.js
+++ lern-react/src/Components/Homework2/Square.js
@@ -1,1 +1,17 @@
+// Передаем пропсы
+
 import React from 'react'
+
+
+class Square extends React.Component {
+    render(){
+        return(
+            <div className= {this.props.cssClass }>
+                { this.props.label }
+            </div>
+        )
+    }
+
+}
+
+export default Square;
