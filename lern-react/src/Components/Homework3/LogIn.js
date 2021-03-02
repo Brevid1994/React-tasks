cf04bb969126b87706200fb59c2653018f3d0087
Brevid1994 <72521095+Brevid1994@users.noreply.github.com>
2021-03-02 20:55:49 +0300

    docs: add LogIn task

---
+++ lern-react/src/Components/Homework3/LogIn.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class LogIn extends React.Component{
+    state = {
+        isSignedIn: false,
+    }
+
+
+    render(){
+        return(
+            <button>Sign In</button>
+        )
+    }
+}
+
+export default LogIn;
