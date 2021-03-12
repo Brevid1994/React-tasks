1d2a8413f1f82ac9bb3ddfed2e83202eb5ea8120
Brevid1994 <72521095+Brevid1994@users.noreply.github.com>
2021-03-12 18:28:13 +0300

    feat:: task with random users done

--- lern-react/src/Components/Practice/Names.js
+++ lern-react/src/Components/Practice/Names.js
@@ -2,22 +2,29 @@ import React from 'react'
 import names from '../../names.json'
 
 class RandomUser extends React.Component {
+
     state = {
-        people: [names.splice('')] 
+        people: ['Bob', 'James', 'Alex']
     }
 
-    addUser = () => {
-        this.setState ( { people: [[...this.state.people, 'Andrei', 'Alex']] } )
-    }
+    onAddRandomName = () => {
 
-    
+        const index = Math.floor(Math.random() * 13)
+
+        const newPeopleList = [...this.state.people, names]
+
+        this.setState({people: newPeopleList })
+
+    }
 
     render () {
         return (
             <div>
-                { this.state.people }
-                <button onClick = { this.addUser }>Add name
-                </button>
+                { this.state.people.map((item) => {
+                return <div>{item}</div>
+                })}
+
+                <button onClick = { this.onAddRandomName } >Add name</button>
             </div>
         )
     }
