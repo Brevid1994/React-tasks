499a51d9ab46b0990b373e972ce7e82fe6efaa62
Brevid1994 <72521095+Brevid1994@users.noreply.github.com>
2021-03-17 19:02:03 +0300

    feat: Task with add random user done

--- lern-react/src/Components/Practice/Names.js
+++ lern-react/src/Components/Practice/Names.js
@@ -1,27 +1,27 @@
 import React from 'react'
-// import names from '../../names.json'
+import names from '../../names.json'
 
 class RandomUser extends React.Component {
 
-    // state = {
-    //     people: ['Bob', 'James', 'Alex']
-    // }
+    state = {
+        people: ['Bob', 'James', 'Alex']
+    }
 
-    // onAddRandomName = () => {
-    //     const index = Math.floor(Math.random() * 13)
-    //     const newPeopleList = [...this.state.people, names]
-    //     this.setState({people: newPeopleList })
-    // }
+    onAddRandomName = () => {
+        const index = Math.floor(Math.random() * 12)
+        const newPeopleList = [...this.state.people, names[index]]
+        this.setState({people: newPeopleList })
+    }
 
     render () {
-        return <p className = 'critical-error'>TASK WITN ADDING RANDOM USER IN PROGRESS</p> 
-            // <div>
-            //     { this.state.people.map((item) => {
-            //         return <div key = { item._id }>{item}</div>
-            //     })}
-            //     <button onClick = { this.onAddRandomName } >Add name</button>
-            // </div>
-        
+        return(
+            <div className = 'random-users-block'>
+                { this.state.people.map((item) => {
+                    return <div key = { item._id }>{item}</div>
+                })}
+                <button onClick = { this.onAddRandomName } >Add name</button>
+            </div>
+        )
     }
 }
 
